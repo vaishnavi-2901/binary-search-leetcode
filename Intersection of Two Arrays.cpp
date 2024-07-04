4e97fe4ea3a3286f07cc114f1421cc9e154c03a7
vaishnavi pandey <36892963+vaishnavi-2901@users.noreply.github.com>
2024-07-04 22:40:25 +0530

    Update Intersection of Two Arrays.cpp

--- Intersection of Two Arrays.cpp
+++ Intersection of Two Arrays.cpp
@@ -21,3 +21,31 @@ Constraints:
 1 <= nums1.length, nums2.length <= 1000
 0 <= nums1[i], nums2[i] <= 1000
 */
+
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int i=0, j=0;
+        vector<int> res;
+        set<int> s;
+        while(i<nums1.size() and j<nums2.size()){
+            if(nums1[i] == nums2[j]){
+                s.insert(nums1[i]);
+                i++, j++;
+            }else if(nums1[i]>nums2[j]){
+                j++;
+            }else{
+                i++;
+            }
+        }
+
+        for(auto e : s){
+            res.push_back(e);
+        }
+        return res;
+    }
+};
